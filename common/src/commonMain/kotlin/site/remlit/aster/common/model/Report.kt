1d0f21db61cc49e7132f2ebc085fac1df8675a7f
ihateblueb <ihateblueb@proton.me>
2025-11-29 12:36:00 -0500

    commit this before making a new branch

---
+++ common/src/commonMain/kotlin/site/remlit/aster/common/model/Report.kt
@@ -0,0 +1,14 @@
+package site.remlit.aster.common.model
+
+import kotlinx.serialization.Serializable
+import kotlin.js.JsExport
+
+@Serializable
+@JsExport
+data class Report(
+	val id: String,
+	val comment: String? = null,
+	val user: User? = null,
+	val note: Note? = null,
+	val instance: Instance? = null
+)
--- frontend/packages/app/src/routes/about.tsx
+++ frontend/packages/app/src/routes/about.tsx
@@ -22,9 +22,20 @@ function RouteComponent() {
                 title={"About"}
             />
             <PageWrapper padding={"full"} center={true}>
-                <b>Version</b>
-                <p>
-                    Aster {data?.version?.aster}<br/>
+                <img alt={"Aster logo"} src={"/favicon.png"} height={"50px"}/>
+
+                <p className={"centerText"}>
+                    Aster {data?.version?.aster}
+                </p>
+
+                {data?.plugins?.length > 0 ? (
+                    <details>
+                        <summary>Plugins</summary>
+                        {JSON.stringify(data?.plugins)}
+                    </details>
+                ) : null}
+
+                <p className={"centerText"}>
                     Kotlin {data?.version?.kotlin}<br/>
                     Runtime {data?.version?.java}<br/>
                     {data?.version?.system}
