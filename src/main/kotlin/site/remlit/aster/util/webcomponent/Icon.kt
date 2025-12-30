9ed73a0d891493cc9bf5baa826f4fe2197cd9830
ihateblueb <ihateblueb@proton.me>
2025-12-30 09:41:52 -0500

    Remove unused broken imports

--- src/main/kotlin/site/remlit/aster/util/webcomponent/Icon.kt
+++ src/main/kotlin/site/remlit/aster/util/webcomponent/Icon.kt
@@ -1,11 +1,8 @@
 package site.remlit.aster.util.webcomponent
 
-import com.github.nwillc.ksvg.elements.SVG
 import kotlinx.html.FlowContent
 import kotlinx.html.classes
 import kotlinx.html.i
-import kotlinx.html.svg
-import kotlinx.html.unsafe
 
 fun FlowContent.ti(name: String) {
 	i {
